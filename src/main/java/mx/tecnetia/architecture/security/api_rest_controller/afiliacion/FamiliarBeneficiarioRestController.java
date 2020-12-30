735ca907ae949e6e063ac15a8bfc3151bfd9d9f1
daniel.garciaca-at-967664872311 <daniel.garciaca-at-967664872311>
2020-12-29 21:00:46 -0600

    adecuacion de rest controller

--- src/main/java/mx/tecnetia/architecture/security/api_rest_controller/afiliacion/FamiliarBeneficiarioRestController.java
+++ src/main/java/mx/tecnetia/architecture/security/api_rest_controller/afiliacion/FamiliarBeneficiarioRestController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import mx.tecnetia.architecture.security.aplicacion.dto.afiliacion.FamiliarBeneficiarioDTO;
+import mx.tecnetia.architecture.security.aplicacion.negocio.service.afiliacion.AfiliacionService;
+import mx.tecnetia.architecture.security.aplicacion.negocio.service.afiliacion.FamiliarBeneficiarioService;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -21,14 +23,16 @@ import mx.tecnetia.architecture.security.aplicacion.dto.afiliacion.FamiliarBenef
 @Log4j2
 public class FamiliarBeneficiarioRestController {
 	
+	private final FamiliarBeneficiarioService familiarBeneficiarioService;
+	
 	@PostMapping("registro")
 	public ResponseEntity<Boolean> nuevo(@RequestBody @Valid FamiliarBeneficiarioDTO familiarBeneficiarioDTO) {
 
 		HttpStatus status = HttpStatus.OK;
 
-		boolean ret = afiliacionService.nuevoFamiliar(nuevoFamiliarDTO);
+		Long ret = familiarBeneficiarioService.saveFamiliarBeneficiario(familiarBeneficiarioDTO);
 
-		return ret ? new ResponseEntity<>(true, status) : new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
+		return ret != null ? new ResponseEntity<>(true, status) : new ResponseEntity<>(false, HttpStatus.INTERNAL_SERVER_ERROR);
 
 	}
 
